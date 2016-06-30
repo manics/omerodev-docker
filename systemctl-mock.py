9e13d7f4b4b85151783134572b97ea2bba257e4b
Simon Li <spli@dundee.ac.uk>
2016-06-30 16:46:36 +0100

    Include postgresql94-check-db-dir in pg start

--- systemctl-mock.py
+++ systemctl-mock.py
@@ -8,7 +8,9 @@ commands = {
     "show -p Environment postgresql-9.4.service":
         "grep ^Environment /usr/lib/systemd/system/postgresql-9.4.service",
     "start postgresql-9.4":
-        "pgrep postgres > /dev/null || su - postgres -c '/usr/pgsql-9.4/bin/pg_ctl start -D /var/lib/pgsql/9.4/data/ -s -w -t 300'",
+        "pgrep postgres > /dev/null || ( \
+            /usr/pgsql-9.4/bin/postgresql94-check-db-dir /var/lib/pgsql/9.4/data/ && \
+            su - postgres -c '/usr/pgsql-9.4/bin/pg_ctl start -D /var/lib/pgsql/9.4/data/ -s -w -t 300' )",
     "stop postgresql-9.4":
         "su - postgres -c '/usr/pgsql-9.4/bin/pg_ctl stop -D /var/lib/pgsql/9.4/data/ -s -m fast'",
     "reload postgresql-9.4":
